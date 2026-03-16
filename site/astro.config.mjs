dfcc5b3599d34bd34cadd676cc117f32c16f83c5
lelafontant <lelafontant@yahoo.ca>
2026-03-16 08:00:12 -0400

    cours terminal removed (temp)

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -121,12 +121,12 @@ export default defineConfig({
 							label: 'Semaine 7',
 							items: [
 								{ label: 'Définition de fonctions', slug: 'cours/07-fonctions-def' },
-								{
-									label: "Pour aller plus loin",
-									items: [
-										{ label: 'Exécution dans le terminal', slug: 'cours/07-terminal' },
-									]
-								},
+								// {
+								// 	label: "Pour aller plus loin",
+								// 	items: [
+								// 		{ label: 'Exécution dans le terminal', slug: 'cours/07-terminal' },
+								// 	]
+								// },
 							]
 						},
 					]
